81358abc97ffcde309aed8e501adb7b74256ff4a
Leandro <leandrodasilvalara@yahoo.com.br>
2022-04-25 16:23:24 -0300

    :boom: Query com subselect
    Query com subselect

---
+++ F_SelecionarTurma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaAlunosFormsApp
+{
+    public partial class F_SelecionarTurma : Form
+    {
+        public F_SelecionarTurma()
+        {
+            InitializeComponent();
+        }
+    }
+}
